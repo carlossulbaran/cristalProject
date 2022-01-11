0b37af8eac30e1f1dd8313f1d1c9cfd3283d0398
carlossulbaran <carlossulbaran11@gmail.com>
2022-01-11 00:00:23 -0400

    ok

--- codigo_supremo_poderosisimo/codigo_general.py
+++ codigo_supremo_poderosisimo/codigo_general.py
@@ -253,7 +253,7 @@ def arduino_rec_info():
 def arduino_env_info(msg):
     if __name__ == '__main__':
         
-        
+        msg = str(msg)
         print('Running. Press CTRL-C to exit.')
         with serial.Serial("/dev/ttyACM0", 9600, timeout=1) as arduino:
             time.sleep(0.1) #wait for serial to open
