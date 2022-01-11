8465694aac2ef8cc5828f573bd5528f19690f554
carlossulbaran <carlossulbaran11@gmail.com>
2022-01-10 23:45:56 -0400

    ok

--- codigo_supremo_poderosisimo/codigo_general.py
+++ codigo_supremo_poderosisimo/codigo_general.py
@@ -265,6 +265,7 @@ def arduino_env_info(msg):
                         for a in np.arange(5):
                             
                             arduino.write(msg.encode())
+                            print(msg.encode())
                             time.sleep(0.1) #wait for arduino to answer
                         break
 
