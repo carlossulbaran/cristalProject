972d46864f1c77cf33e8ff6d62e63ea8d9b54c79
carlossulbaran <carlossulbaran11@gmail.com>
2022-01-12 11:11:52 -0400

    ok

--- codigo_supremo_poderosisimo/codigo_general.py
+++ codigo_supremo_poderosisimo/codigo_general.py
@@ -355,6 +355,9 @@ def mapa_trabajo(ancho,largo,posicion_muestras,ubicacion,contd,conti,m_izv,m_der
         
         #calcular el angulo de error
         pos_obj = int(input("pos_obj = "))
+        print(posicion_muestras[pos_obj,:])
+        print(ubicacion)
+        
         ang_gi_rad = angulo(ang,ubicacion, posicion_muestras,pos_obj)
         print(ang_gi_rad)
 
