1ae39ad89d95c4e7e32bacc02a7061e642fbc019
carlossulbaran <carlossulbaran11@gmail.com>
2022-01-13 20:07:42 -0400

    ok

--- codigo_supremo_poderosisimo/codigo_general.py
+++ codigo_supremo_poderosisimo/codigo_general.py
@@ -422,14 +422,14 @@ def calculo_velocidades(angulo_gi_rad,ubicacion,posicion_muestras):
 #Manda el angulo de movimiento al servo usando PCA al servo 15 especificamente
 def servo(ang_servo):
     print(kit.servo[13].angle)
-    if kit.servo[13].angle == 40:
+    if kit.servo[13].angle <= 40:
         angn = 40
         for a in np.arange(8):
             kit.servo[13].angle = angn
             angn = angn + 10
             time.sleep(0.1)
 
-    if kit.servo[13].angle == 120:
+    if kit.servo[13].angle >= 60:
         angn = 120
         for a in np.arange(8):
             kit.servo[13].angle = angn
