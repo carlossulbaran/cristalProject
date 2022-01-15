a4bf426fb17477fd131438688b89af867654f83d
carlossulbaran <carlossulbaran11@gmail.com>
2022-01-15 01:13:34 -0400

    ok

--- codigo_supremo_poderosisimo/codigo_general.py
+++ codigo_supremo_poderosisimo/codigo_general.py
@@ -549,8 +549,8 @@ def calcular_posicion(ubicacion,ang,vel_li):
 
     #calculos de orientacion
     orientacion = ang + w*(t-tv)
-    x = ubicacion[0] + v*cos(orientacion))*(t-tv)
-    y = ubicacion[1] + v*sen(orientacion)*(t-tv)
+    x = ubicacion[0] + ((v*cos(orientacion)*(t-tv))
+    y = ubicacion[1] + (v*sen(orientacion)*(t-tv))
 
     tv=t
 
