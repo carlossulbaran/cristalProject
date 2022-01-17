580845d19403d9750a871c66d7380d4b2bfd981d
carlossulbaran <carlossulbaran11@gmail.com>
2022-01-17 12:45:50 -0400

    ok

--- codigo_supremo_poderosisimo/codigo_general.py
+++ codigo_supremo_poderosisimo/codigo_general.py
@@ -495,7 +495,7 @@ def encoders():
     #tiempo
     t = time.process_time()
 
-    while (contd < 40) and (conti < 40):
+    while (contd < 20) and (conti < 20):
         m_der = gpio.input(5)
 
         if (m_der == 1 and m_derv == 0):
