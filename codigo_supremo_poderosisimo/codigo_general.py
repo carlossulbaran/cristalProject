187f3aeb715f1e86326385810e81bdb6c86e6411
carlossulbaran <carlossulbaran11@gmail.com>
2022-01-19 16:51:43 -0400

    ok

--- codigo_supremo_poderosisimo/codigo_general.py
+++ codigo_supremo_poderosisimo/codigo_general.py
@@ -377,7 +377,7 @@ def mapa_trabajo(ancho,largo,posicion_muestras,ubicacion,contd,conti,m_izv,m_der
     #inicializar distancia
     d = 1000
 
-    print(posicion_muestras(pos_obj,:))
+    print(posicion_muestras)
 
     running = True
     tv = time.process_time()
