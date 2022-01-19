074b50e150a106a804b78648b2651c70561dc860
carlossulbaran <carlossulbaran11@gmail.com>
2022-01-19 17:23:53 -0400

    ok

--- codigo_supremo_poderosisimo/codigo_general.py
+++ codigo_supremo_poderosisimo/codigo_general.py
@@ -653,7 +653,7 @@ def calcular_posicion(ubicacion,ang,vr,vl,t,tv):
     #print("vr = "+str(vr))
     #print("vl = "+str(vl))
 
-    w = ((vl-vr)/b)/2   #rad/s
+    w = ((vl-vr)/b)/4   #rad/s
     v = (vr+vl)/2   #m/s
 
     #print("w = "+str(w))
