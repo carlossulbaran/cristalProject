071c83f739945677f267c5e25477513d2f4b48df
carlossulbaran <carlossulbaran11@gmail.com>
2022-01-19 19:35:51 -0400

    ok

--- codigo_supremo_poderosisimo/codigo_general.py
+++ codigo_supremo_poderosisimo/codigo_general.py
@@ -495,7 +495,7 @@ def calculo_velocidades(angulo_gi_rad,ubicacion,posicion_muestras):
     #print(angulo_gi_rad)
 
     #k es una constante para calibrar las ecuaciones
-    k=0.2
+    k=0.4
     #calculo de distancias entre el robot y el target
     d = mt.sqrt(((posicion_muestras[pos_obj,0]-ubicacion[0])**2)+((posicion_muestras[pos_obj,1]-ubicacion[1])**2))
 
