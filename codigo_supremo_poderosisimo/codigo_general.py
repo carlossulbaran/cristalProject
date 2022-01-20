22e383ad19477382813ffea7555ee35b7522aca1
carlossulbaran <carlossulbaran11@gmail.com>
2022-01-19 20:38:24 -0400

    ok

--- codigo_supremo_poderosisimo/codigo_general.py
+++ codigo_supremo_poderosisimo/codigo_general.py
@@ -425,7 +425,7 @@ def mapa_trabajo(ancho,largo,posicion_muestras,ubicacion,contd,conti,m_izv,m_der
         elif d<=0.4:
             pos_obj = pos_obj + 1
 
-        #print(ubicacion)
+        print(ubicacion)
         #print("orientacion = "+str(ang))
         print(posicion_muestras[pos_obj,:])
         
