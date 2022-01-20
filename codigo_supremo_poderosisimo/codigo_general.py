5650092555646f815e7762b8860943210f8acf82
carlossulbaran <carlossulbaran11@gmail.com>
2022-01-20 17:51:37 -0400

    ok

--- codigo_supremo_poderosisimo/codigo_general.py
+++ codigo_supremo_poderosisimo/codigo_general.py
@@ -434,7 +434,7 @@ def mapa_trabajo(ancho,largo,posicion_muestras,ubicacion,contd,conti,m_izv,m_der
         actualizar_pos(ubicacion,posicion_muestras,ancho,largo,screen1)
         
         #Si estamos cerca del obj sensar y cambiar el obj 20cm de tolerancia
-        if d <= 0.7 and pos_obj == 0:
+        if d <= 0.8 and pos_obj == 0:
             #contg, resultados, pos_obj = sensar(contg,resultados)
             pos_obj = pos_obj + 1
             time.sleep(3)
